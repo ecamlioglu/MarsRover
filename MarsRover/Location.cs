0c98a0202da76ddb2484971b8512deb6a66152e8
Erdem Camlioglu <secamlioglu@gmail.com>
2020-07-28 15:30:15 +0300

    project

---
+++ MarsRover/Location.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MarsRover {
+        public enum Directions {
+            N = 0,
+            S = 1,
+            E = 2,
+            W = 3
+        }
+        public interface IDirection {
+            void Move(List<int> ranges, string direction);
+        }
+        public class Location : IDirection {
+            public int xLoc { get; set; }
+            public int yLoc { get; set; }
+            public Directions directionRanger { get; set; }
+
+            public Location()
+            {
+                xLoc = yLoc = 0;
+                directionRanger = Directions.N;
+            }
+            /*          N
+             *        W R E
+             *          S
+             */         
+            private void TurnLeft()
+            {
+                switch (this.directionRanger)
+                {
+                    case Directions.N:
+                        this.directionRanger = Directions.W;
+                        break;
+                    case Directions.S:
+                        this.directionRanger = Directions.E;
+                        break;
+                    case Directions.E:
+                        this.directionRanger = Directions.N;
+                        break;
+                    case Directions.W:
+                        this.directionRanger = Directions.S;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            private void TurnRight()
+            {
+                switch (this.directionRanger)
+                {
+                    case Directions.N:
+                        this.directionRanger = Directions.E;
+                        break;
+                    case Directions.S:
+                        this.directionRanger = Directions.W;
+                        break;
+                    case Directions.E:
+                        this.directionRanger = Directions.S;
+                        break;
+                    case Directions.W:
+                        this.directionRanger = Directions.N;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            private void Forward()
+            {
+                switch (this.directionRanger)
+                {
+                    case Directions.N:
+                        this.yLoc += 1;
+                        break;
+                    case Directions.S:
+                        this.yLoc -= 1;
+                        break;
+                    case Directions.E:
+                        this.xLoc += 1;
+                        break;
+                    case Directions.W:
+                        this.xLoc -= 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            public void Move(List<int> Ranges, string move)
+            {
+                for (int i = 0; i < move.Length; i++)
+                {
+                    switch (move[i])
+                    {
+                        case 'M':
+                            this.Forward();
+                            break;
+                        case 'R':
+                            this.TurnRight();
+                            break;
+                        case 'L':
+                            this.TurnLeft();
+                            break;
+                        default:
+                            break;
+                    }
+                    if (this.xLoc < 0 || this.yLoc < 0 || this.yLoc > Ranges[1] || this.xLoc > Ranges[0])
+                        Console.WriteLine("ERROR!! LOCATION FAULT .. by Location.cs");
+                }
+            }
+        }
+    
+}
---
+++ MarsRover/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MarsRover {
+    class Program {
+        static void Main(string[] args)
+        {
+            bool roverDone = false;
+            int roverCount = 1;
+            var maxCoordinate = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
+            while (!roverDone)
+            {
+                Console.WriteLine(roverCount + ". rover");
+                var startPos = Console.ReadLine().Trim().Split(' ');
+                Location location = new Location();
+
+
+                if (startPos.Count() == 3)
+                {
+                    location.xLoc = Convert.ToInt32(startPos[0]);
+                    location.yLoc = Convert.ToInt32(startPos[1]);
+                    location.directionRanger = (Directions)Enum.Parse(typeof(Directions), startPos[2]);
+                }
+            
+                var move = Console.ReadLine().ToUpper();
+                try
+                {
+                    location.Move(maxCoordinate, move);
+                    Console.WriteLine($"{location.xLoc} {location.yLoc} {location.directionRanger.ToString()}");
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                    throw;
+                }
+                Console.WriteLine("U want to enter another rover in this plateau? (yes or no)");
+                if(Console.ReadLine() == "yes")
+                {
+                    roverDone = false;
+                    roverCount += 1;
+                }
+                else
+                {
+                    roverDone = true;
+                    System.Environment.Exit(-1);
+                }
+
+            }
+            
+        }
+    }
+}
